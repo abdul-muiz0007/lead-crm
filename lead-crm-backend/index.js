6fcb582b9a276c38b13d8bfa975d56de3b5f2e76
abdul-muiz0007 <abdulmuiz5086@gmail.com>
2025-08-10 22:47:53 +0100

    Deploy command update

--- lead-crm-backend/index.js
+++ lead-crm-backend/index.js
@@ -1,14 +1,8 @@
-import path from 'path';
-import { fileURLToPath } from 'url';
-
 const express = require("express");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
 const mongoose = require("mongoose");
 require("dotenv").config();
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const app = express();
 app.use(cors());
@@ -25,12 +19,6 @@ const userRoutes = require("./routes/userRoutes");
 app.use("/api/leads", leadRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
-
-app.use(express.static(path.join(__dirname, '../frontend/build')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
-});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
